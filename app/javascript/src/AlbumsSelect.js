5218073ed678b6c358df5472a9b1924a5a863677
Dennis B <bluz71@users.noreply.github.com>
2020-06-17 14:52:42 +1000

    Finishing up Albums page

--- app/javascript/src/AlbumsSelect.js
+++ app/javascript/src/AlbumsSelect.js
@@ -9,13 +9,33 @@ class AlbumsSelect {
       '[data-behavior~=album-search-link-target]',
       this.searchClick
     )
+    $(document).on(
+      'click',
+      '[data-behavior~=album-filter-link-target]',
+      this.filterClick
+    )
+    $(document).on(
+      'click',
+      '[data-behavior~=album-filter-select-target]',
+      this.filterSelectClick
+    )
   }
 
   searchClick = event => {
+    $('[data-behavior~=album-filter-form-target]').slideUp(250)
     $('[data-behavior~=album-search-form-target]').slideToggle(
       250,
       this.searchFocus
     )
+  }
+
+  filterClick = event => {
+    $('[data-behavior~=album-search-form-target]').slideUp(250)
+    $('[data-behavior~=album-filter-form-target]').slideToggle(250)
+  }
+
+  filterSelectClick = event => {
+    $('[data-behavior~=album-filter-select-target]').addClass('is-loading')
   }
 
   searchFocus = () => {
