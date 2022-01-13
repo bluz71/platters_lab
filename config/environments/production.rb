d02d099e43cb7e9dd70cc1ba147d83b9f74f7ee4
Dennis B <bluz71@users.noreply.github.com>
2022-01-13 12:11:34 +1100

    Minor tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "bulma_sampler_production"
+  # config.active_job.queue_name_prefix = "platters_lab_production"
 
   config.action_mailer.perform_caching = false
 
