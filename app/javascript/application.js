60d92bd9dcf02331599b89d4d9ea3ff1bdf9b1b4
Dennis B <bluz71@users.noreply.github.com>
2023-04-02 15:38:23 +1000

    Replace jQuery comment interaction with Alpine.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
-import CommentInteraction from './src/CommentInteraction'
-
 // Third-party packages.
 import '@hotwired/turbo-rails'
 
@@ -11,16 +9,5 @@ Alpine.plugin(collapse)
 window.Alpine = Alpine
 Alpine.start()
 
-// jQuery import and global exposure.
-const jQuery = require('jquery')
-window.$ = window.jQuery = jQuery
-
 // Third-party packages.
 require('@rails/ujs').start()
-
-// Event handlers to run once the DOM is ready.
-$(() => {
-  /* eslint-disable no-new */
-  new CommentInteraction()
-  /* eslint-enable no-new */
-})
